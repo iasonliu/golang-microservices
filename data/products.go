742af5eff7abacdc0b56172053458510bcf98d35
Xiangxu Liu <xxliu@toughtworks.com>
2021-03-04 14:41:07 +0800

    json tags

--- data/products.go
+++ data/products.go
@@ -4,14 +4,14 @@ import "time"
 
 // Product defines the structure for an API product
 type Product struct {
-	ID          int
-	Name        string
-	Description string
-	Price       float32
-	SKU         string
-	CreatedOn   string
-	UpdatedOn   string
-	DeletedOn   string
+	ID          int     `json:"id"`
+	Name        string  `json:"name"`
+	Description string  `json:"description"`
+	Price       float32 `json:"price"`
+	SKU         string  `json:"sku"`
+	CreatedOn   string  `json:"-"`
+	UpdatedOn   string  `json:"-"`
+	DeletedOn   string  `json:"-"`
 }
 
 func GetProducts() []*Product {
