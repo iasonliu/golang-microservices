787ae3117b2ccc8386a59e0fd380b299c79afa8b
Xiangxu Liu <xxliu@toughtworks.com>
2021-03-04 10:32:00 +0800

    gracefu shutdown http

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
 	"time"
 
 	"github.com/iasonliu/working/handlers"
@@ -24,5 +26,22 @@ func main() {
 		ReadTimeout:  1 * time.Second,
 		WriteTimeout: 1 * time.Second,
 	}
-	logger.Fatal(httpServer.ListenAndServe())
+
+	go func() {
+		err := httpServer.ListenAndServe()
+		if err != nil {
+			logger.Fatalln(err)
+		}
+	}()
+	sigChan := make(chan os.Signal)
+	signal.Notify(sigChan, os.Interrupt)
+	signal.Notify(sigChan, os.Kill)
+	sig := <-sigChan
+	logger.Println("Recieved terminate, graceful shutdown", sig)
+
+	timeoutCtx, err := context.WithTimeout(context.Background(), 30*time.Second)
+	if err != nil {
+		logger.Println(err)
+	}
+	logger.Fatal(httpServer.Shutdown(timeoutCtx))
 }
